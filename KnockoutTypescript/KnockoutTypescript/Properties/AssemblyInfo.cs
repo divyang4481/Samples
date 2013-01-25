5df14872e139c440fab56d361eee719e9d5765db
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-01-25 23:53:51 +0100

    Removed Models dir

--- KnockoutTypescript/KnockoutTypescript/Properties/AssemblyInfo.cs
+++ KnockoutTypescript/KnockoutTypescript/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MvcTypeScriptFirst")]
+[assembly: AssemblyTitle("KnockoutTypescript")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MvcTypeScriptFirst")]
+[assembly: AssemblyProduct("KnockoutTypescript")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
