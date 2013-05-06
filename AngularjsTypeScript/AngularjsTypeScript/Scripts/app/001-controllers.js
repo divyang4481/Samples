f706cc58258593dc45d36dc154606226252cc55f
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-05-06 22:58:34 +0200

    Added order by

--- AngularjsTypeScript/AngularjsTypeScript/Scripts/app/001-controllers.js
+++ AngularjsTypeScript/AngularjsTypeScript/Scripts/app/001-controllers.js
@@ -1,9 +1,14 @@
 ﻿var ParticleList;
 (function (ParticleList) {
     var Particle = (function () {
-        function Particle(name, symbol) {
+        function Particle(name, symbol, massMeV, subsymbol, supsymbol) {
+            if (typeof subsymbol === "undefined") { subsymbol = ""; }
+            if (typeof supsymbol === "undefined") { supsymbol = ""; }
             this.name = name;
             this.symbol = symbol;
+            this.massMeV = massMeV;
+            this.subsymbol = subsymbol;
+            this.supsymbol = supsymbol;
         }
         return Particle;
     })();
@@ -11,9 +16,12 @@
     var ParticleCtrl = (function () {
         function ParticleCtrl($scope) {
             $scope.particles = [
-                new Particle("mion", "μ"), 
-                new Particle("taon", "τ"), 
-                new Particle("electron", "e")
+                new Particle("muon", "μ", 105.7, "", "-"), 
+                new Particle("taon", "τ", 1777, "", "-"), 
+                new Particle("electron", "e", 0.511, "", "-"), 
+                new Particle("neutrino (electron)", "ν", 0, "e"), 
+                new Particle("neutrino (muon)", "ν", 0, "μ"), 
+                new Particle("neutrino (taon)", "ν", 0, "τ")
             ];
             $scope.title = "Leptons";
         }
--- AngularjsTypeScript/AngularjsTypeScript/Scripts/app/001-controllers.ts
+++ AngularjsTypeScript/AngularjsTypeScript/Scripts/app/001-controllers.ts
@@ -5,7 +5,8 @@ module ParticleList
 {
     export class Particle
     {
-        constructor(public name: string, public symbol: string) {
+        constructor(public name: string, public symbol: string, public massMeV: number,
+             public subsymbol: string = "", public supsymbol: string = "") {
         }
     }
 
@@ -17,9 +18,12 @@ module ParticleList
     export class ParticleCtrl {
         constructor($scope: Scope) {
             $scope.particles = [
-                new Particle("mion", "μ"),
-                new Particle("taon", "τ"),
-                new Particle("electron", "e")
+                new Particle("muon", "μ", 105.7, "", "-"),
+                new Particle("taon", "τ", 1777, "", "-"),
+                new Particle("electron", "e", 0.511, "", "-"),
+                new Particle("neutrino (electron)", "ν", 0, "e"),
+                new Particle("neutrino (muon)", "ν", 0, "μ"),
+                new Particle("neutrino (taon)", "ν", 0, "τ")
             ];
 
             $scope.title = "Leptons";
