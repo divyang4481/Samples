f640c5b48f4d8ab9c6dbc76dc0dd56666644e1ee
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-05-13 23:29:15 +0200

    OSIM project structure, TicTacToe: first test

---
+++ TDD/TicTacToe/TicTacToe.UnitTests/GameWinnerServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TicTacToe.UnitTests
+{
+    public class GameWinnerServiceTests
+    {
+        public class ValidateMethod
+        {
+            [Fact]
+            public void neither_player_should_win_when_board_is_empty()
+            {
+                // Arrange
+                IGameWinnerService target;
+                const char expected = ' ';
+                var gameBoard = new[,]
+                    {
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '}
+                    };
+
+                // Act
+                var result = target.Validate(gameBoard);
+
+                // Assert
+                Assert.Equal(expected, result);
+            }
+        }
+    }
+}
