e0a74dff31afbf0adefeb1549df64d0ffcafd6ca
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-05-15 00:43:14 +0200

    Test does no pass

--- TDD/TicTacToe/TicTacToe.Services/GameWinnerService.cs
+++ TDD/TicTacToe/TicTacToe.Services/GameWinnerService.cs
@@ -4,17 +4,30 @@
     {
         public char Validate(char[,] gameBoard)
         {
+            var currentWinningSymbol = ' ';
+            currentWinningSymbol = CheckForThreeInRowInHorizontalRow(gameBoard);
+            currentWinningSymbol = CheckForThreeInARowInVerticalColumn(gameBoard);
+            return currentWinningSymbol;
+        }
+
+        private char CheckForThreeInARowInVerticalColumn(char[,] gameBoard)
+        {
+            var rowOneChar = gameBoard[0, 0];
+            var rowTwoChar = gameBoard[1, 0];
+            var rowThreeChar = gameBoard[2, 0];
+            if (rowOneChar == rowTwoChar && rowTwoChar == rowThreeChar)
+            {
+                return rowOneChar;
+            }
+            return ' ';
+        }
+
+        private char CheckForThreeInRowInHorizontalRow(char[,] gameBoard)
+        {
             var columnOneChar = gameBoard[0, 0];
             var columnTwoChart = gameBoard[0, 1];
             var columnThreeChar = gameBoard[0, 2];
             if (columnOneChar == columnTwoChart && columnTwoChart == columnThreeChar)
-            {
-                return columnOneChar;
-            }
-
-            var rowTwoChar = gameBoard[1, 0];
-            var rowThreeChar = gameBoard[2, 0];
-            if (columnOneChar == rowTwoChar && rowTwoChar == rowThreeChar)
             {
                 return columnOneChar;
             }
