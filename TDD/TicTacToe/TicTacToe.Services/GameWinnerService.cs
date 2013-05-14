0ea63815ed05317c5979f8dbef2a33f709c1a263
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-05-15 01:00:33 +0200

    Refactoring

--- TDD/TicTacToe/TicTacToe.Services/GameWinnerService.cs
+++ TDD/TicTacToe/TicTacToe.Services/GameWinnerService.cs
@@ -2,63 +2,63 @@
 {
     public class GameWinnerService : IGameWinnerService
     {
+        private char[,] gameBoard;
+
         private const char SymbolForNoWinner = ' ';
 
         public char Validate(char[,] gameBoard)
         {
-            var currentWinningSymbol = CheckForThreeInRowInHorizontalRow(gameBoard);
+            this.gameBoard = gameBoard;
+
+            var currentWinningSymbol = CheckForThreeInRowInHorizontalRow();
             if (currentWinningSymbol != SymbolForNoWinner)
             {
                 return currentWinningSymbol;
             }
 
-            currentWinningSymbol = CheckForThreeInRowDiagonally(gameBoard);
+            currentWinningSymbol = CheckForThreeInRowDiagonally();
             if (currentWinningSymbol != SymbolForNoWinner)
             {
                 return currentWinningSymbol;
             }
 
-            currentWinningSymbol = CheckForThreeInARowInVerticalColumn(gameBoard);
+            currentWinningSymbol = CheckForThreeInARowInVerticalColumn();
             return currentWinningSymbol;
         }
 
-        private char CheckForThreeInARowInVerticalColumn(char[,] gameBoard)
+        private char CheckForThreeInARowInVerticalColumn()
         {
             var rowOneChar = gameBoard[0, 0];
             var rowTwoChar = gameBoard[1, 0];
             var rowThreeChar = gameBoard[2, 0];
-            if (rowOneChar == rowTwoChar && rowTwoChar == rowThreeChar)
-            {
-                return rowOneChar;
-            }
-            return SymbolForNoWinner;
+
+            return AreSymbolsAreEqual(rowOneChar, rowTwoChar, rowThreeChar);
         }
 
-        private char CheckForThreeInRowInHorizontalRow(char[,] gameBoard)
+        private char CheckForThreeInRowInHorizontalRow()
         {
             var columnOneChar = gameBoard[0, 0];
             var columnTwoChart = gameBoard[0, 1];
             var columnThreeChar = gameBoard[0, 2];
-            if (columnOneChar == columnTwoChart && columnTwoChart == columnThreeChar)
-            {
-                return columnOneChar;
-            }
 
-            return SymbolForNoWinner;
+            return AreSymbolsAreEqual(columnOneChar, columnTwoChart, columnThreeChar);
         }
 
-        private char CheckForThreeInRowDiagonally(char[,] gameBoard)
+        private char CheckForThreeInRowDiagonally()
         {
             var cellOneChar = gameBoard[0, 0];
             var cellTwoChar = gameBoard[1, 1];
-            var cellThreChar = gameBoard[2, 2];
+            var cellThreeChar = gameBoard[2, 2];
 
-            if (cellOneChar == cellTwoChar && cellTwoChar == cellThreChar)
-            {
-                return cellOneChar;
-            }
-
-            return SymbolForNoWinner;
+            return AreSymbolsAreEqual(cellOneChar, cellTwoChar, cellThreeChar);
         }
+
+        private char AreSymbolsAreEqual(char firstSymbol, char secondSymbol, char thirdSymbol)
+        {
+            return (firstSymbol == secondSymbol && secondSymbol == thirdSymbol)
+                       ? firstSymbol
+                       : SymbolForNoWinner;
+        }
+
     }
 }
