72440b417ea252b5ad1830aeb5edfe7150577b89
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-05-14 23:28:22 +0200

    Made the second test pass

--- TDD/TicTacToe/TicTacToe.Services/GameWinnerService.cs
+++ TDD/TicTacToe/TicTacToe.Services/GameWinnerService.cs
@@ -6,6 +6,14 @@ namespace TicTacToe.Services
     {
         public char Validate(char[,] gameBoard)
         {
+            var columnOneChar = gameBoard[0, 0];
+            var columnTwoChart = gameBoard[0, 1];
+            var columnThreeChar = gameBoard[0, 2];
+            if (columnOneChar == columnTwoChart && columnTwoChart == columnThreeChar)
+            {
+                return columnOneChar;
+            }
+
             return ' ';
         }
     }
