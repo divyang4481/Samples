fd40026c5bd6a1cde5baa07459ce8672df50df6d
Piotr Ludwiczuk <piotrosz@gmail.com>
2013-07-12 00:30:33 +0200

    Added StudentRepository

---
+++ EntityFramework/ContosoUniversity/ContosoUniversity/DAL/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ContosoUniversity.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ContosoUniversity.DAL
+{
+    public interface IStudentRepository : IDisposable
+    {
+        IQueryable<Student> GetStudents();
+        Student GetStudentById(int studentId);
+        void InsertStudent(Student student);
+        void DeleteStudent(int studentId);
+        void UpdateStudent(Student student);
+        void Save();
+    }
+}
---
+++ EntityFramework/ContosoUniversity/ContosoUniversity/DAL/StudentRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class StudentRepository : IStudentRepository, IDisposable
+    {
+        private SchoolContext context;
+
+        public StudentRepository(SchoolContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Student> GetStudents()
+        {
+            return context.Students;
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return context.Students.Find(id);
+        }
+
+        public void InsertStudent(Student student)
+        {
+            context.Students.Add(student);
+        }
+
+        public void DeleteStudent(int studentId)
+        {
+            Student student = context.Students.Find(studentId);
+            context.Students.Remove(student);
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            context.Entry(student).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
