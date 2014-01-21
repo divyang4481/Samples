89f7dfa385dc89b19181576354b55fc590b05118
Piotr Ludwiczuk <piotrosz@gmail.com>
2014-01-21 12:04:20 +0100

    Added self-hosting example (console app)

---
+++ Katana/KatanaGettingStarted/OwinHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using KatanaGettingStarted;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int port = 5000;
+
+            using(Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:" + port))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Listening on port {0}", port);
+                Console.WriteLine("Press [enter] to quit");
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Katana/KatanaGettingStarted/OwinHost/Properties/AssemblyInfo.cs
+++ Katana/KatanaGettingStarted/OwinHost/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KatanaGettingStarted")]
+[assembly: AssemblyTitle("OwinHost")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KatanaGettingStarted")]
-[assembly: AssemblyCopyright("Copyright © Piotr Ludwiczuk 2014")]
+[assembly: AssemblyCompany("Axa Direct Solutions SAS S A Uproszczona Oddział w Polsce")]
+[assembly: AssemblyProduct("OwinHost")]
+[assembly: AssemblyCopyright("Copyright © Axa Direct Solutions SAS S A Uproszczona Oddział w Polsce 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5c2fda58-3b78-41e6-8c3f-1a80ad032c5a")]
+[assembly: Guid("9336c107-09fd-4126-bbfe-73863f440296")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
