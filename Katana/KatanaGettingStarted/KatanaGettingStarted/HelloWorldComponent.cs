ee6c2d74a91b666fe44a1a1fa5799c0a9b1764b1
Piotr Ludwiczuk <piotrosz@gmail.com>
2014-01-24 16:27:15 +0100

    Added HelloWorldComponent Katana component

---
+++ Katana/KatanaGettingStarted/KatanaGettingStarted/HelloWorldComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace KatanaGettingStarted
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class HelloWorldComponent
+    {
+        // Naxt component in the pipeline
+        AppFunc _next;
+
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            await _next(environment);
+        }
+    }
+}
--- Katana/KatanaGettingStarted/KatanaGettingStarted/Startup.cs
+++ Katana/KatanaGettingStarted/KatanaGettingStarted/Startup.cs
@@ -11,8 +11,9 @@ namespace KatanaGettingStarted
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Use<HelloWorldComponent>();
 
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             app.Run(context =>
             {
                 context.Response.ContentType = "text/plain";
